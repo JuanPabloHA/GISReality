67514ae47f065501450b161a43d36e6eacb3063d
Juan Pablo <juan.p.herreraalvarez@student.uts.edu.au>
2019-09-09 16:26:37 +1000

    Libraries and network import

--- Networkanalysis.py
+++ Networkanalysis.py
@@ -1,3 +1,15 @@
 # Network Analysis
 # Juan Pablo Herrera Alvarez
 # iLab 1 - GIS Reality / UTS Startups
+
+# Import libraries
+import os
+#import matplotlib.pyplot as plt
+#import networkx as nx
+
+# Defines working environment
+print(os.getcwd())
+os.chdir("/Users/juanpablo/OneDrive/UTS/iLab1/QGIS/Network/ClipData")
+
+# Imports pedestrian network
+#G = nx.read_shp('Roads.shp')
