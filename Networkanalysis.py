f29de26fad688fc695618983989cab4bc9e00d0a
Juan Pablo <juan.p.herreraalvarez@student.uts.edu.au>
2019-09-10 19:20:29 +1000

    import the network into python

--- Networkanalysis.py
+++ Networkanalysis.py
@@ -4,12 +4,22 @@
 
 # Import libraries
 import os
-#import matplotlib.pyplot as plt
-#import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+import gdal 
 
 # Defines working environment
 print(os.getcwd())
 os.chdir("/Users/juanpablo/OneDrive/UTS/iLab1/QGIS/Network/ClipData")
 
 # Imports pedestrian network
-#G = nx.read_shp('Roads.shp')
+G = nx.read_shp('Roads.shp')
+G2 = nx.read_shp('Roads.shp', simplify=True)
+
+# Convert the network to matrix format 
+pNetwork = nx.to_numpy_matrix(G)
+pNetwork.shape[1]
+
+pNetwork2 = nx.to_numpy_matrix(G2)
+pNetwork2.shape[1]
